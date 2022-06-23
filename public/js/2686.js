7157a00c6cce148f57622ae4f5fff90a01a517cc
Julian Tabona <brandontabona@gmail.com>
2022-06-23 14:34:55 +0200

    JS Minified for production

---
+++ public/js/2686.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[2686],{12686:(e,n,a)=>{a.r(n),a.d(n,{default:()=>l});var t=a(94865);const c={data:function(){return{account:this.$page.props.accountPayload}}};const l=(0,a(83744).Z)(c,[["render",function(e,n,a,c,l,o){return(0,t.openBlock)(),(0,t.createElementBlock)("div",null,"Global Variables")}]])}}]);
